96aad7b5935dd4c9b1b14d9a468d77f1658921cd
Alex Tran <vietnogi@gmail.com>
2015-08-12 13:25:10 -0700

    modal directive

---
+++ dist/example.js
@@ -0,0 +1,58 @@
+(function() {
+  'use strict';
+  var config;
+
+  config = function($stateProvider) {
+    var key, results, state, states;
+    states = {};
+    states['avatar'] = {
+      url: '/',
+      title: 'Avatar',
+      templateUrl: 'views/avatar.html'
+    };
+    states['loader'] = {
+      url: '/loader',
+      templateUrl: 'views/loader.html'
+    };
+    states['filters'] = {
+      url: '/filters',
+      templateUrl: 'views/filters.html'
+    };
+    states['modal'] = {
+      url: '/modal',
+      templateUrl: 'views/modal.html',
+      controller: 'ModalExampleController as vm'
+    };
+    results = [];
+    for (key in states) {
+      state = states[key];
+      results.push($stateProvider.state(key, state));
+    }
+    return results;
+  };
+
+  config.$inject = ['$stateProvider'];
+
+  angular.module('appirio-tech-ng-ui-components').config(config).run();
+
+}).call(this);
+
+(function() {
+  'use strict';
+  var ModalExampleController;
+
+  ModalExampleController = function($scope) {
+    var activate, vm;
+    vm = this;
+    vm.show = false;
+    activate = function() {
+      return vm;
+    };
+    return activate();
+  };
+
+  ModalExampleController.$inject = ['$scope'];
+
+  angular.module('appirio-tech-ng-ui-components').controller('ModalExampleController', ModalExampleController);
+
+}).call(this);
