73f5a9aeb09ec50f47a091e2daa676e20b74d04c
Alex Tran <vietnogi@gmail.com>
2015-10-08 18:24:45 -0700

    height for ie bug fix - build

--- dist/main.js
+++ dist/main.js
@@ -281,7 +281,8 @@ $templateCache.put("views/selected-button.directive.html","<button ng-class=\"{\
       var heightDiff, top;
       top = getOffsetTop($element[0]);
       heightDiff = viewPortHeight - top;
-      return $element.css('min-height', heightDiff + 'px');
+      $element.css('min-height', heightDiff + 'px');
+      return $element.css('height', heightDiff + 'px');
     };
     $($window).bind('resize', function() {
       var element, i, len, results;
@@ -325,7 +326,8 @@ $templateCache.put("views/selected-button.directive.html","<button ng-class=\"{\
     };
     setViewPortHeight();
     fullHeight = function($element) {
-      return $element.css('min-height', viewPortHeight + 'px');
+      $element.css('min-height', viewPortHeight + 'px');
+      return $element.css('height', viewPortHeight + 'px');
     };
     $($window).bind('resize', function() {
       var element, i, len, results;
