9e2b045b44dfa02d96d85261e73570fd9fbaf762
Alex Tran <vietnogi@gmail.com>
2015-12-18 14:45:00 -0800

    remove web pack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,3 @@
 require('./node_modules/coffee-script/register');
 
-module.exports = require('./webpack.config.coffee')(__dirname);
+module.exports = require('appirio-tech-webpack-config')(__dirname);
